12d3e356691bee143e2fa9e8881fde41d27bc6ce
jyotiranjan <jsatapathy007@gmail.com>
2020-06-26 20:07:35 +0530

    My protfolia app initial commit

---
+++ rp-portfolio/projects/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.conf.urls import include
+from . import views
+
+
+urlpatterns = [
+    path("", views.project_index, name="project_index"),
+    path("<int:pk>/", views.project_detail, name="project_detail"),
+    path("blog/", include("blog.urls")),
+]
